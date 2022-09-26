552b11760fceee03925ad7c34c72b137e44de79c
Alejandro-houlu <alejandro.hlcw@gmail.com>
2022-09-26 15:01:57 +0800

    'first'

---
+++ emailAddressBook_backend/src/main/java/com/myEmailAddressBook/emailAddressBook_backend/EmailAddressBookBackendApplication.java
@@ -0,0 +1,13 @@
+package com.myEmailAddressBook.emailAddressBook_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailAddressBookBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailAddressBookBackendApplication.class, args);
+	}
+
+}
